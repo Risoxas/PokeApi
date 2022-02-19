690ecd099bf6a1439e0cede4a86ff46440596a5c
Risoxas <davidag93@gmail.com>
2022-02-19 13:47:09 -0600

    Fixing style error and adding searchbox

--- src/Components/PokemonList.js
+++ src/Components/PokemonList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import "../styles/_list.scss";
 import axios from "axios";
 import PokemonCard from "./PokemonCard";
@@ -7,12 +8,24 @@ import {
   Pagination,
   PaginationItem,
   PaginationLink,
+  Form,
+  Col,
+  Label,
+  Input,
+  Button,
+  Alert,
 } from "reactstrap";
+
 export default function PokemonList() {
+  const [loading, setLoading] = useState(true);
   const [currentPage, setcurrentPage] = useState(1);
   const [data, setData] = useState(null);
   const [totalPages, setTotalPages] = useState(0);
   const [pagination, setPagination] = useState([]);
+  const [search, setSearch] = useState(null);
+  const [result, setResult] = useState(false);
+  const [show, setShow] = useState(false);
+  const baseUrl = "https://pokeapi.co/api/v2/pokemon";
 
   const handlePage = (e) => {
     switch (e.target.id) {
@@ -29,18 +42,13 @@ export default function PokemonList() {
         setcurrentPage(totalPages);
         break;
       default:
-        console.log(e.target.id);
         setcurrentPage(parseInt(e.target.id));
         break;
     }
   };
   useEffect(() => {
     axios
-      .get(
-        `https://pokeapi.co/api/v2/pokemon?offset=${
-          (currentPage - 1) * 18
-        }&limit=18`
-      )
+      .get(`${baseUrl}?offset=${(currentPage - 1) * 18}&limit=18`)
       .then((response) => {
         setData(response.data);
 
@@ -50,47 +58,108 @@ export default function PokemonList() {
           let arr = Array.from({ length: pages + 1 }, (_, i) => i);
           let start = currentPage >= pages - 4 ? pages - 4 : currentPage;
           setPagination(arr.slice(start, start + 5));
+          setLoading(false);
         }
       });
   }, [currentPage]);
 
-  if (!data) return null;
+  const handleChange = (e) => {
+    setShow(false);
+    const { value } = e.target;
+    if (!value) {
+      setResult(false);
+      setSearch(null);
+    } else {
+      setSearch({ name: value, url: `${baseUrl}/${value}` });
+    }
+  };
+  const handleBack = (e) => {
+    let input = document.getElementById("searchBox");
+    input.value = "";
+    setSearch(null);
+    setResult(false);
+  };
+  const handleSearch = () => {
+    if (search) {
+      setResult(search);
+    } else {
+      setShow(true);
+    }
+  };
+
+  if (loading) return null;
+
   return (
     <>
+      <Form>
+        <Col md={2}>
+          <Label for="searchBox">Buscar</Label>
+          <div className="d-flex">
+            <Input
+              type="text"
+              id="searchBox"
+              name="search"
+              onSubmit={handleBack}
+              onChange={handleChange}
+            />
+            <Button color="primary" onClick={handleSearch}>
+              <FaSearch />
+            </Button>
+          </div>
+        </Col>
+        {show ? (
+          <Alert variant="danger" isOpen={show}>
+            Please add a value in searchbox
+          </Alert>
+        ) : null}
+      </Form>
       <CardGroup>
-        {data.results.map((item) => (
-          <PokemonCard key={item.id} pokemon={item} />
-        ))}
+        {result ? (
+          <PokemonCard pokemon={result} />
+        ) : (
+          data.results.map((item) => (
+            <PokemonCard key={item.id} pokemon={item} />
+          ))
+        )}
       </CardGroup>
-      <Pagination className="pagination">
-        <PaginationItem key="first" disabled={currentPage === 1}>
-          <PaginationLink id="first" href="#" onClick={handlePage}>
-            First
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem key="prev" disabled={currentPage === 1}>
-          <PaginationLink onClick={handlePage} id="prev">
-            Prev
-          </PaginationLink>
-        </PaginationItem>
-        {pagination.map((item, idx) => (
-          <PaginationItem key={currentPage + idx} active={currentPage === item}>
-            <PaginationLink onClick={handlePage} id={item}>
-              {item}
+      {result ? (
+        <Button color="primary" onClick={handleBack}>
+          Atras
+        </Button>
+      ) : (
+        <Pagination className="pagination">
+          <PaginationItem key="first" disabled={currentPage === 1}>
+            <PaginationLink id="first" href="#" onClick={handlePage}>
+              First
             </PaginationLink>
           </PaginationItem>
-        ))}
-        <PaginationItem disabled={currentPage === totalPages}>
-          <PaginationLink id="next" href="#" onClick={handlePage}>
-            Next
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem disabled={currentPage === totalPages}>
-          <PaginationLink id="last" href="#" onClick={handlePage}>
-            Last
-          </PaginationLink>
-        </PaginationItem>
-      </Pagination>
+          <PaginationItem key="prev" disabled={currentPage === 1}>
+            <PaginationLink onClick={handlePage} id="prev">
+              Prev
+            </PaginationLink>
+          </PaginationItem>
+          {pagination.map((item, idx) => (
+            <PaginationItem
+              key={currentPage + idx}
+              active={currentPage === item}
+            >
+              <PaginationLink onClick={handlePage} id={item}>
+                {item}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationItem disabled={currentPage === totalPages}>
+            <PaginationLink id="next" href="#" onClick={handlePage}>
+              Next
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem disabled={currentPage === totalPages}>
+            <PaginationLink id="last" href="#" onClick={handlePage}>
+              Last
+            </PaginationLink>
+          </PaginationItem>
+        </Pagination>
+      )}
     </>
   );
 }
